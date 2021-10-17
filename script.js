c336bc53877f926401764e770bdb2edb64b30639
vincentasVilk <70482769+vincentasVilk@users.noreply.github.com>
2021-10-17 23:14:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var characters = ["Jon Snow","Carsei Lannister","Daenerys Targaryen","Theon Greyjoy","Tyrion Lannister","Arya Stark"];
+console.log(characters[0]);
+console.log(characters[5]);
+
+var message = prompt("Iveskite skaiciu nuo 1 iki 6");
